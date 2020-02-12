670894a513c62013b49dbc66c8d25a0165c58199
tenpages <tenpages66@gmail.com>
2020-02-12 00:56:37 -0500

    remove test prints

--- master.py
+++ master.py
@@ -321,10 +321,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
     def _median_of_means(self):
         b = math.floor(self.num_workers / (2*self._s+0.5))
         for g_idx, grads in enumerate(self._grad_aggregate_buffer):
-            print(type(grads))
-            print(type(grads[0]))
             median = np.median(np.array([np.mean(np.array(grads[i:i+b]), axis=0) for i in range(0,self.num_workers,b)]), axis=0)
-            print(median)
             self._grad_aggregate_buffer[g_idx] = median
 
     def _grad_norm(self):
