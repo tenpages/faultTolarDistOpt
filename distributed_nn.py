b8a5abb1b46f295805f0fd94fa4849a024b8b11d
Shuo Liu <tenpages66@163.com>
2020-02-27 14:52:23 -0500

    dataset seed

--- distributed_nn.py
+++ distributed_nn.py
@@ -125,8 +125,6 @@ def load_data(dataset, seed, args, rank, world_size):
                     transforms.Normalize((0.1307,), (0.3081,))
                 ]))
                 train_loader = torch.utils.data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
-                print(list(train_loader)[0])
-                assert (False)
         test_loader = None
         return train_loader, training_set, test_loader
 
