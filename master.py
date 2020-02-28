0d9fbefdaa2cc5386fb8b0d3f80cb489e2050587
Shuo Liu <tenpages66@163.com>
2020-02-28 16:11:36 -0500

    test

--- master.py
+++ master.py
@@ -239,7 +239,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
             self._grad_aggregate_buffer[layer_idx] += gradient
         elif self._update_mode in ("geometric_median", "krum", 'multi_krum', 'coor_wise_median', 'coor_wise_trimmed_mean',
                                    'median_of_means', 'grad_norm', 'grad_norm_coor_wise', 'grad_norm_full_grad'):
-            print(self._grad_aggregate_buffer[layer_idx][source].shape)
+            print(self._grad_aggregate_buffer[layer_idx][source].shape, gradient.shape)
             self._grad_aggregate_buffer[layer_idx][source] = gradient
             """
             _shape = gradient.shape
