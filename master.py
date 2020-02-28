0aa89c6c4f42584493e1afaa739bec8e205c1dd3
Shuo Liu <tenpages66@163.com>
2020-02-28 16:25:18 -0500

    test

--- master.py
+++ master.py
@@ -445,6 +445,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
         print(np.sqrt(np.sum(np.square([np.linalg.norm(self._grad_aggregate_buffer[i], axis=1) for i in range(len(self._grad_aggregate_buffer))]), axis=0)))
         print(np.linalg.norm(concatenated_gradients, axis=1))
         print(np.mean(np.linalg.norm(concatenated_gradients, axis=1)))
+        print(np.linalg.norm(np.mean(concatenated_gradients, axis=0)))
         for i in range(self.num_workers-self._s, self.num_workers):
             concatenated_gradients[ranks[i]] = concatenated_gradients[ranks[i]]*norm/np.linalg.norm(concatenated_gradients[ranks[i]])
         sum_gradient = np.mean(np.array(concatenated_gradients), axis=0)
