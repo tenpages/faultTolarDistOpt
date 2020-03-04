b3502c1d214d4787d764648985e31557a170d86e
tenpages <tenpages66@gmail.com>
2020-03-04 01:40:16 -0500

    grad clip drop f

--- master.py
+++ master.py
@@ -68,6 +68,9 @@ class SyncReplicaMaster_NN(NN_Trainer):
     def start(self):
         self.async_bcast_step()
 
+        if self._checkpoint_step != 0:
+            torch.set_rng_state(torch.load(self._train_dir+"rng_state_"+str(self.checkpoint_step)))
+        
         for i in range(self._checkpoint_step + 1, self._max_steps + 1):
             self.network.train()
             self._first_grad_received = False
