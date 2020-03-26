ad5a5afdd604aa30a5e2cd30f80d49cb6dfb9696
tenpages <tenpages66@gmail.com>
2020-03-25 22:36:18 -0400

    new dataset

--- distributed_eval.py
+++ distributed_eval.py
@@ -137,6 +137,14 @@ if __name__ == "__main__":
         testing_set=torch.load("linRegDataset")
         test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
         data_shape = testing_set[0][0].size()[0]
+    elif args.dataset == "LinReg2":
+        testing_set=torch.load("linRegDataset2")
+        test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
+        data_shape = testing_set[0][0].size()[0]
+    elif args.dataset == "LinReg3":
+        testing_set=torch.load("linRegDataset3")
+        test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
+        data_shape = testing_set[0][0].size()[0]
 
     print("testing set loaded.")
 
