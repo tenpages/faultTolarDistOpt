de2564fe56d8b68bfe9cc07c02983a3608208956
tenpages <tenpages66@gmail.com>
2020-03-30 12:34:15 -0400

    evaluator

--- distributed_eval.py
+++ distributed_eval.py
@@ -136,11 +136,11 @@ if __name__ == "__main__":
         test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
         data_shape = testing_set[0][0].size()[0]*testing_set[0][0].size()[1]*testing_set[0][0].size()[2]
     elif args.dataset == "CIFAR10":
-        training_set = datasets.CIFAR10('./cifar10_data', train=False, transform=transforms.Compose([
+        testing_set = datasets.CIFAR10('./cifar10_data', train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ]))
-        train_loader = torch.utils.data.DataLoader(training_set, batch_size=args.eval_batch_size, shuffle=True)
+        test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
         data_shape = testing_set[0][0].size()[0]*testing_set[0][0].size()[1]*testing_set[0][0].size()[2]
     print("testing set loaded.")
 
