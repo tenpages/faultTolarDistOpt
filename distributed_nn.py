36b52a8e59481eb3558f9f4d45cb7ef773888cb3
tenpages <tenpages66@gmail.com>
2020-04-30 23:33:36 -0400

    geomedian fault

--- distributed_nn.py
+++ distributed_nn.py
@@ -246,7 +246,7 @@ def _generate_adversarial_nodes(args, world_size):
         adversaries = [i*b for i in range(args.worker_fail)]
         print(b, adversaries)
         assert len(adversaries) == args.worker_fail
-        return adversaries * (args.max_steps + 1)
+        return [adversaries] * (args.max_steps + 1)
 
 
 def prepare(args, rank, world_size):
