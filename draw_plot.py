4ca72084690bb3789115da01104c156be4ba3106
tenpages <tenpages66@gmail.com>
2020-05-14 22:39:02 -0400

    update drawing that extract data from file

--- draw_plot.py
+++ draw_plot.py
@@ -11,7 +11,7 @@ model_names = ['normal', 'geomedian', 'medofmeans', 'cwtm', 'mkrum5', 'normfilte
 #model_names = ['normal', 'grad_norm-drop-f', 'grad_norm_full_grad-drop-f']
 #model_names = ['normal', 'krum', 'multi_krum-5']
 #model_names = ['100','40','25','10','5']
-current_step = 0
+
 new_ticks = np.linspace(0,1000,1001)
 plt.figure(figsize=(10,9), dpi=1200)
 plt.xlim(0,1000)
@@ -33,7 +33,7 @@ for model_name in model_names:
     results = np.load(filename)
     step = results[0].astype(int)
     loss = results[1]
-    prec1 = resulsts[2]
+    prec1 = results[2]
     prec3 = results[3]
     plt.plot(step,loss,label=model_name,linewidth=.1)
     losses.append(loss)
@@ -41,65 +41,64 @@ for model_name in model_names:
 plt.legend(loc="right")
 plt.ylim(0,2.5)
 plt.savefig("results/loss-mnist-16-10-geomedian.pdf")
-plt.xlim(10000,60000)
-plt.savefig("results/loss-normal-1-2.pdf")
 plt.clf()
 
+new_ticks = np.linspace(0,1000,1001)
 plt.figure(figsize=(10,9), dpi=1200)
-plt.xlim(0,1220)
-plt.ylim(0,100)
+plt.xlim(0,1000)
+#plt.ylim(-1,1)
 #plt.xticks(new_ticks)
 plt.xlabel("Step")
-plt.ylabel("Precision")
+plt.ylabel("Loss")
+losses=[]
 
 for model_name in model_names:
     step=[]
-    prec1=[]
+    loss=[]
     if model_name=='normal':
-        filename = 'results-' + model_name + '-41-0-1200'
+        filename = 'output/models/paper1/MNIST-LeNet/16/' + model_name + '/40-0/results.npy'
     else:
-        filename = 'results-'+model_name+'-41-10-1200'
+    	filename = 'output/models/paper1/MNIST-LeNet/16/geomedian/' + model_name + '/40-10/results.npy'
+    #filename = 'results-normal-1-'+model_name+'-0-60000'
     print(model_name)
-    with open(filename,"r") as f:
-        for line in f:
-            t=re.match("Evaluator evaluating results on step (\d+)\D+",line)
-            if t!=None:
-                current_step = int(t.groups()[0])
-            else:
-                t=re.match("Test set: Average loss: (.+), Prec@1: (\d+\.\d+) Prec@5: (\d+\.\d+)",line)
-                if t!=None:
-                    current_prec_1 = float(t.groups()[1])
-                    step.append(current_step)
-                    prec1.append(current_prec_1)
-    plt.plot(step,prec1,label=model_name)
+    results = np.load(filename)
+    step = results[0].astype(int)
+    loss = results[1]
+    prec1 = results[2]
+    prec3 = results[3]
+    plt.plot(step,prec1,label=model_name,linewidth=.1)
+    losses.append(loss)
 
 plt.legend(loc="right")
-plt.savefig("prec1.pdf")
+plt.savefig("results/prec1-mnist-16-10-geomedian.pdf")
 plt.clf()
 
+new_ticks = np.linspace(0,1000,1001)
 plt.figure(figsize=(10,9), dpi=1200)
-plt.xlim(0,1220)
-plt.ylim(0,100)
+plt.xlim(0,1000)
+#plt.ylim(-1,1)
 #plt.xticks(new_ticks)
 plt.xlabel("Step")
-plt.ylabel("Precision")
+plt.ylabel("Loss")
+losses=[]
+
 for model_name in model_names:
     step=[]
-    prec5=[]
-    filename = 'results-'+model_name+'-41-2-1200'
+    loss=[]
+    if model_name=='normal':
+        filename = 'output/models/paper1/MNIST-LeNet/16/' + model_name + '/40-0/results.npy'
+    else:
+    	filename = 'output/models/paper1/MNIST-LeNet/16/geomedian/' + model_name + '/40-10/results.npy'
+    #filename = 'results-normal-1-'+model_name+'-0-60000'
     print(model_name)
-    with open(filename,"r") as f:
-        for line in f:
-            t=re.match("Evaluator evaluating results on step (\d+)\D+",line)
-            if t!=None:
-                current_step = int(t.groups()[0])
-            else:
-                t=re.match("Test set: Average loss: (.+), Prec@1: (\d+\.\d+) Prec@5: (\d+\.\d+)",line)
-                if t!=None:
-                    current_prec_5 = float(t.groups()[2])
-                    step.append(current_step)
-                    prec5.append(current_prec_5)
-    plt.plot(step,prec5,label=model_name)
+    results = np.load(filename)
+    step = results[0].astype(int)
+    loss = results[1]
+    prec1 = results[2]
+    prec3 = results[3]
+    plt.plot(step,loss,label=model_name,linewidth=.1)
+    losses.append(loss)
 
 plt.legend(loc="right")
-plt.savefig("prec5.pdf", dpi=800)
+plt.savefig("results/prec3-mnist-16-10-geomedian.pdf")
+plt.clf()
