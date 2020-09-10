284a1162fbc2eb1a8c8af5107267e43c5ff82312
tenpages <tenpages66@gmail.com>
2020-09-10 18:26:23 -0400

    multiple run script

--- multiple_run.py
+++ multiple_run.py
@@ -9,7 +9,7 @@ for i in nums_faults:
 	for fault_type in fault_types:
 		for model_name, model in zip(model_names, models):
 			if i == 2 and fault_type == 'revgrad2' and model_name != 'mkrum5':
-				print(fault_type+" "+model_names+" "+str(i)+"/40 skipped")
+				print(fault_type+" "+model_name+" "+str(i)+"/40 skipped")
 				continue
 			args = ['mpirun', '-n', '41', 
 					'python', 'distributed_nn.py', 
