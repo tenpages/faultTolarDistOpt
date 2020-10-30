fixed aggregated gradient averaging

master2.py
@@ -122,6 +122,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
         self.async_bcast_step()
         if self._redundancy :
             self.bcast_seed()
+            self.bcast_rand_nums()
 
         if self._checkpoint_step != 0:
             # torch.set_rng_state(torch.load(self._train_dir+"rng_state_"+str(self._checkpoint_step)))
@@ -153,8 +154,10 @@ class SyncReplicaMaster_NN(NN_Trainer):
                 dp_list, worker_list = (None,None)
                 # print("_q == {} {}".format(self._q,type(self._q)))
                 if self._q == 1.0 or fault_check:
+                    self._num_grad_to_collect = (self.batch_size)*(self._s + 1)
                     dp_list, worker_list = self.async_bcast_datapoints_redundant()
                 else:
+                    self._num_grad_to_collect = self.batch_size
                     dp_list, worker_list = self.async_bcast_datapoints_singular()
 
                 self.set_coded_buffer(worker_list)
@@ -177,7 +180,8 @@ class SyncReplicaMaster_NN(NN_Trainer):
                     print("Master step {} check for faults".format(self.cur_step))
                     for dpidx, dp_buffer in enumerate(self.coded_buffer):
                         dp_flag = True
-                        first=dp_buffer[0] 
+                        first=dp_buffer[0]
+                        # print("dp_buffer[0]",dp_buffer[0])
 
                         for widx, worker_buffer in enumerate(dp_buffer):
                             for layer_idx, param in enumerate(worker_buffer):
@@ -194,7 +198,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
 
                 if faulty_grad_datapoints:
                     print(f"Master step {self.cur_step} datapoints with faulty gradients: {faulty_grad_datapoints}") 
-
+                    # self._num_grad_to_collect = (self._s+1)*self.batch_size + self._s
                     # redistribute datapoints for second round 
                     # rule: datapoints must go to different workers than before
                     new_dp_list, new_worker_list = self.async_bcast_redundant_datapoints(dp_list, faulty_grad_datapoints)
@@ -265,15 +269,11 @@ class SyncReplicaMaster_NN(NN_Trainer):
                                         if rank not in faulty_worker_ranks:
                                             faulty_worker_ranks.append(rank)
 
-                            print(f"Master step {self.cur_step} faulty workers from dp {dpidx} : {curr_faulty_workers}")
+                            # print(f"Master step {self.cur_step} faulty workers from dp {dpidx} : {curr_faulty_workers}")
                             
                     print(f"Master: step {self.cur_step} faulty worker ranks: ",faulty_worker_ranks)
 
                     # aggregate gradients of all non-faulty workers
-                    """
-                        FIX THIS
-                        NEEDS TO ONLY GET a single GRADIENT FROM NON-FAULTY WORKERS
-                    """
                     for dpidx, grad_list in enumerate(self.coded_buffer):
                         used_grads_t = used_grads_t + 1
                         for widx, grad in enumerate(grad_list):
@@ -291,8 +291,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
 
                 else :
                     # either skipped fault check or found no faulty gradients
-                    if fault_check:
-                        print(f"Master {self.cur_step} no faulty gradients")
+                    # send blank lists to skip redundant step
                     redundancy_requests=[]
                     for rank in range(1,self.world_size):
                         # send an empty list to each worker with tag=9
@@ -300,37 +299,44 @@ class SyncReplicaMaster_NN(NN_Trainer):
                             redundancy_requests.append(self.comm.isend([], dest=rank, tag=8))
                     for i in range(len(redundancy_requests)):
                         redundancy_requests[i].wait()
-
+                    count = 0
+                    # aggregate gradient
                     for dpidx, grad_list in enumerate(self.coded_buffer):
                         # tally total gradients used for update
                         # for each dpidx, accumulated grads are averaged
                         used_grads_t = used_grads_t + 1
-                        total_grads_t = total_grads_t + 1
 
                         if len(grad_list) == 1:
+                            total_grads_t = total_grads_t + 1
+                            grad = grad_list[0]
+                            count += 1
                             for layer_index, param in enumerate(self.network.parameters()):
-                                self.aggregate_gradient(gradient=grad_list[0][layer_index], layer_idx=layer_index, source=worker_list[dpidx][0] - 1)
+                                if layer_index < 2:
+                                    print("[{}] worker={} layer={}\t{}".format(self.cur_step,worker_list[dpidx][0],layer_index,grad[layer_index].tolist()))
+                                self.aggregate_gradient(gradient=grad[layer_index], layer_idx=layer_index, source=worker_list[dpidx][0] - 1)
                         else:
+                            if dpidx == 0:
+                                print(f"Master {self.cur_step} no faulty gradients")
+                            """
                             temp_grad=[]
                             for layer_index, param in enumerate(self.network.parameters()):
                                 temp_grad.append(np.zeros(param.size()))
                             for widx, grad in enumerate(grad_list):
                                 for layer_index, param in enumerate(self.network.parameters()):
-                                    # self.aggregate_gradient(gradient=grad[layer_index], layer_idx=layer_index, source=worker_list[dpidx][widx] - 1)
                                     temp_grad[layer_index] += grad[layer_index]
-                            print(temp_grad, len(grad_list))
-                            temp_grad /= len(grad_list)
+                            print("grad_list {}\ngrad_list[0] {}\ngrad_list[0][0] {}".format(len(grad_list),len(grad_list[0]),len(grad_list[0][0])))
+                            for idx, grad in enumerate(temp_grad):
+                                temp_grad[idx] /= len(grad_list)
                             print(temp_grad) 
                             for layer_idx, grad in enumerate(temp_grad):
                                 self.aggregate_gradient(gradient=grad,layer_idx=layer_idx,source=worker_list[dpidx][0]-1)
+                            """
 
-                        """
-                        for widx, grad in enumerate(grad_list):
-                            # add to total gradients calculated, greater than or equal to (batch_size) * (f+1)
-                            total_grads_t = total_grads_t + 1
-                            for layer_index, param in enumerate(self.network.parameters()):
-                                self.aggregate_gradient(gradient=grad[layer_index], layer_idx=layer_index, source=worker_list[dpidx][widx] - 1)
-                        """
+                            for widx, grad in enumerate(grad_list):
+                                # add to total gradients calculated, greater than or equal to (batch_size) * (f+1)
+                                total_grads_t = total_grads_t + 1
+                                for layer_index, param in enumerate(self.network.parameters()):
+                                    self.aggregate_gradient(gradient=grad[layer_index], layer_idx=layer_index, source=worker_list[dpidx][widx] - 1)
 
                     print(f"Master {self.cur_step} used gradients: {used_grads_t}/{total_grads_t}")
                 
@@ -382,6 +388,8 @@ class SyncReplicaMaster_NN(NN_Trainer):
 
                         # aggregate the gradient
                         if self.grad_accumulator.gradient_aggregate_counter[layer_index] <= self._num_grad_to_collect:
+                            if layer_index < 2:
+                                print("[{}] worker={} layer={}\t{}".format(self.cur_step,status.source,layer_index,received_grad.tolist()))
                             self.aggregate_gradient(gradient=received_grad, layer_idx=layer_index, source=status.source-1)
 
                         self.grad_accumulator.gradient_aggregate_counter[layer_index] += 1
@@ -408,9 +416,8 @@ class SyncReplicaMaster_NN(NN_Trainer):
             # update by given gradient filter
             if self._redundancy:
                 method_start = time.time()
-                # average grads for each datapoint 
-                #self._avg_received_grads_n(total_grads_t)
-                print("redundant gradient update")
+                # self._avg_received_grads_n(total_grads_t)
+                self._avg_received_grads()
                 method_duration = time.time() - method_start
             elif self._update_mode == 'normal':
                 method_start = time.time()
@@ -599,6 +606,16 @@ class SyncReplicaMaster_NN(NN_Trainer):
                 req_list.append(self.comm.isend(man_seed, dest=i,tag=7))
         for i in range(len(req_list)):
             req_list[i].wait()
+
+    def bcast_rand_nums(self):
+        req_list = []
+        for i in range(self.world_size):
+            if i != 0:
+                nums = [randn() for i in range(self._max_steps)]
+                req_list.append(self.comm.isend(nums, dest=i,tag=6))
+        for i in range(len(req_list)):
+            req_list[i].wait()
+        
 
     def async_bcast_layer_weights_async(self):
         request_layers = []
@@ -1020,6 +1037,7 @@ class SyncReplicaMaster_NN(NN_Trainer):
         print('Testset performance: Cur step:{}\tPrec@1: {}\tPrec@5: {}'.format(self.cur_step, prec1, prec5))
 
     def _avg_received_grads(self):
+        print("[{}] avg over {}".format(self.cur_step, self._num_grad_to_collect))
         for i in range(len(self._grad_aggregate_buffer)):
             self._grad_aggregate_buffer[i] /= self._num_grad_to_collect
 

worker.py
@@ -34,6 +34,7 @@ class DistributedWorker(NN_Trainer):
         self.cur_step = 0
         self.next_step = 0
 
+        self.rand_nums = []
         self.redundancy = kwargs['redundancy']
         self.red_seed = -1
         self._p = kwargs['p']
@@ -115,6 +116,7 @@ class DistributedWorker(NN_Trainer):
 
         if self.redundancy:
             self.fetch_seed()                   # set self.red_seed
+            self.fetch_rand_nums()
             assert (self.red_seed >= 0)
             torch.manual_seed(self.red_seed)    # set manual seed for data shuffling
 
@@ -148,19 +150,21 @@ class DistributedWorker(NN_Trainer):
 
                         # receive empty list if  blacklisted
                         if not dp_list.tolist():
-                            # print("Worker {} step {} received no datapoints".format(self.rank,self.cur_step))
+                            print("Worker {} step {} received no datapoints".format(self.rank,self.cur_step))
                             red_flag = True
                         else :
                             print(f"Worker {self.rank} step {self.cur_step} datapoints {dp_list.tolist()}")
 
                 X_batch, y_batch = Variable(train_input_batch), Variable(train_label_batch)
                 # print("Worker {} labels {}".format(self.rank, y_batch.tolist()))
-                
+
                 if self.redundancy and not red_flag:
                         X_batch = torch.index_select(train_input_batch,0,dp_list) 
                         y_batch = torch.index_select(train_label_batch,0,dp_list) 
                         X_batch = Variable(X_batch)
                         y_batch = Variable(y_batch)
+                # print("Worker {} x's {}".format(self.rank, X_batch.tolist()))
+                # print("Worker {} labels {}".format(self.rank, y_batch.tolist()))
                         
                 while True:
                     self.async_fetch_step()
@@ -220,6 +224,7 @@ class DistributedWorker(NN_Trainer):
                     loss_list = []
                     forward_start_time = time.time()
                     logits = self.network(X_batch)
+                    print("Worker {} logits: {}".format(self.rank,logits))
                     if self.redundancy and not red_flag:
                         loss = self.criterion(logits,y_batch)
                         numlayers = 0
@@ -227,7 +232,8 @@ class DistributedWorker(NN_Trainer):
                             numlayers = numlayers + 1
                         send_check_requests=[]
 
-                        send_err = self.p_decision(self._p)       # if send_err == 1 then send error
+                        # send_err = self.p_decision(self._p)       # if send_err == 1 then send error
+                        send_err = self.p_decision(self._p,self.rand_nums.pop(0))
 
                         if self.rank in self._fail_workers[self.cur_step] and self._p and send_err:
                             print("Worker {} step {} sending faulty gradient to master".format(self.rank,self.cur_step))
@@ -375,15 +381,20 @@ class DistributedWorker(NN_Trainer):
         seed = req.wait()
         self.red_seed = seed
 
-    def p_decision(self, p) :
+    def fetch_rand_nums(self):
+        req = self.comm.irecv(source=0,tag=6)
+        nums = req.wait()
+        self.rand_nums = nums
+
+    def p_decision(self, p, r) :
         mu = 0.00
         sigma = 1.00
 
-        r = randn()
-
+        # r = randn()
+        _r = r
         quantile = mu + sigma * sqrt(2)*erfinv(2*p-1)
 
-        if quantile >= r :
+        if quantile >= _r :
             return 1
         else:
             return 0
@@ -436,7 +447,6 @@ class DistributedWorker(NN_Trainer):
                 model_counter_ += 1
             new_state_dict.update(tmp_dict)
         self.network.load_state_dict(new_state_dict)
-        print("[{}] worker update".format(self.rank))
     """
     _multi_backward() no longer used
         
@@ -615,3 +625,31 @@ def err_simulation(grad, mode, cyclic=False):
         else:
             return ADVERSARY_ * grad
 
+
+def make_decision(p) :
+    mu = 0.00
+    sigma = 1.00
+
+    r = randn()
+
+    quantile = mu + sigma * sqrt(2)*erfinv(2*p-1)
+
+    if quantile >= r :
+        return 1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    p = .25
+    x=0
+    y=0
+
+    for i in range(1000):
+        if make_decision(p):
+            if x==0:
+                print("first x @ i={}".format(i))
+            x+=1
+        else:
+            y+=1
+
+    print("x = {}%".format(x/1000))