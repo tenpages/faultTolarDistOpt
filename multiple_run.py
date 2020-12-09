36ad677fe6dc9eee4d593ed5b93ab9295fc27c98
tenpages <tenpages66@gmail.com>
2020-12-09 01:45:07 -0500

    multi run

--- multiple_run.py
+++ multiple_run.py
@@ -8,13 +8,14 @@ import subprocess
 model_names = ['normfilter']
 models = ['grad_norm']
 fault_types = ['rev_grad_2']
+fault_names = ['revgrad2']
 nums_faults = [2]
 batch_sizes = ['32','128','256']
 #acc_alphas = ['20','40','60']
 
 for batch_size in batch_sizes:
 	for i in nums_faults:
-		for fault_type in fault_types:
+		for fault_type, fault_name in zip(fault_types, fault_names):
 			for model_name, model in zip(model_names, models):
 				args = ['mpirun', '-n', '41', 
 						'python', 'distributed_nn.py', 
@@ -27,7 +28,7 @@ for batch_size in batch_sizes:
 						'--approach', 'baseline', 
 						'--err-mode', fault_type, 
 						'--lr', '0.01', 
-						'--train-dir', 'output/models/paper2/MNIST-LeNet/' + batch_size + '/' + fault_type + '/' + model_name + '/40-' + str(i) + '/', 
+						'--train-dir', 'output/models/paper2/MNIST-LeNet/' + batch_size + '/' + fault_name + '/' + model_name + '/40-' + str(i) + '/',
 						'--accumulative', 'False', 
 						#'--accumulative-alpha', '0.'+acc_alpha, 
 						'--worker-fail', str(i), 
@@ -36,11 +37,11 @@ for batch_size in batch_sizes:
 						'--calculate-cosine', 'False', 
 						#'--checkpoint-step', '500', 
 						'--eval-freq', '1']
-				print("Now running experiments on "+fault_type+" using "+model_name+" using command:")
+				print("Now running experiments on "+fault_name+" using "+model_name+" using command:")
 				print(' '.join(args))
 				results = subprocess.run(args, capture_output=True)
 				if results.returncode==0 and results.stdout != None:
-					with open('logs-paper2-mnist-' + batch_size + '-' + fault_type + '-' + model_name + '-40-'+str(i),'w') as f:
+					with open('logs-paper2-mnist-' + batch_size + '-' + fault_name + '-' + model_name + '-40-'+str(i),'w') as f:
 						f.write(results.stdout.decode())
 					print("finished")
 					print("========================")
@@ -52,10 +53,10 @@ for batch_size in batch_sizes:
 print()
 for batch_size in batch_sizes:
 	for i in nums_faults:
-		for fault_type in fault_types:
+		for fault_type, fault_name in zip(fault_types, fault_names):
 			for model_name, model in zip(model_names, models):
-				args = 'python distributed_eval.py --model-dir output/models/paper2/MNIST-LeNet/'+batch_size+'/'+fault_type+'/'+model_name \
-					+'/40-'+str(i)+'/ --dataset MNIST --network LeNet --eval-freq 1 > results-paper2-mnist-'+batch_size+'-'+fault_type+'-'+model_name+'-40-'+str(i)+' 2>&1 &'
-				print("Now evaluating "+fault_type+" using "+model_name+" using command:")
+				args = 'python distributed_eval.py --model-dir output/models/paper2/MNIST-LeNet/'+batch_size+'/'+fault_name+'/'+model_name \
+					+'/40-'+str(i)+'/ --dataset MNIST --network LeNet --eval-freq 1 > results-paper2-mnist-'+batch_size+'-'+fault_name+'-'+model_name+'-40-'+str(i)+' 2>&1 &'
+				print("Now evaluating "+fault_name+" using "+model_name+" using command:")
 				print(args)
 				results = subprocess.run(args, shell=True)
