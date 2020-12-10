bbbe28104555db01f8be2bc955473675f3b2d65c
tenpages <tenpages66@gmail.com>
2020-12-10 00:10:41 -0500

    multi run for cifar-64-revgrad2 redo and -labelflipping-40-8, -normfilter-40-8

--- multiple_run.py
+++ multiple_run.py
@@ -17,39 +17,38 @@ for batch_size in batch_sizes:
 	for i in nums_faults:
 		for fault_type, fault_name in zip(fault_types, fault_names):
 			for model_name, model in zip(model_names, models):
-				for acc_alpha in acc_alphas:
-					args = ['mpirun', '-n', '41',
-							'python', 'distributed_nn.py',
-							'--batch-size=' + batch_size,
-							'--max-steps', '500',
-							'--epochs', '100',
-							'--network', 'LeNet',
-							'--mode', model, '--multi-krum-m','5',
-							'--dataset', 'CIFAR10',
-							'--approach', 'baseline',
-							'--err-mode', fault_type,
-							'--lr', '0.01',
-							'--train-dir', 'output/models/paper2/CIFAR10-LeNet/' + batch_size + '/' + fault_name + '/' + model_name + '/40-' + str(i) + '/',
-							'--accumulative', 'False',
-							#'--accumulative-alpha', '0.'+acc_alpha,
-							'--worker-fail', str(i),
-							'--fault-thrshld', str(i),
-							'--data-distribution', 'same',
-							'--calculate-cosine', 'False',
-							#'--checkpoint-step', '500',
-							'--eval-freq', '1']
-					print("Now running experiments on "+fault_name+" using "+model_name+" using command:")
-					print(' '.join(args))
-					results = subprocess.run(args, capture_output=True)
-					if results.returncode==0 and results.stdout != None:
-						with open('logs-paper2-cifar-' + batch_size + '-' + fault_name + '-' + model_name + '-40-' + str(i) + '-acc-' + acc_alpha,'w') as f:
-							f.write(results.stdout.decode())
-						print("finished")
-						print("========================")
-					else:
-						print(results.stderr.decode())
-						print("failed")
-						print("========================")
+				args = ['mpirun', '-n', '41',
+						'python', 'distributed_nn.py',
+						'--batch-size=' + batch_size,
+						'--max-steps', '500',
+						'--epochs', '100',
+						'--network', 'LeNet',
+						'--mode', model, '--multi-krum-m','5',
+						'--dataset', 'CIFAR10',
+						'--approach', 'baseline',
+						'--err-mode', fault_type,
+						'--lr', '0.01',
+						'--train-dir', 'output/models/paper2/CIFAR10-LeNet/' + batch_size + '/' + fault_name + '/' + model_name + '/40-' + str(i) + '/',
+						'--accumulative', 'False',
+						#'--accumulative-alpha', '0.'+acc_alpha,
+						'--worker-fail', str(i),
+						'--fault-thrshld', str(i),
+						'--data-distribution', 'same',
+						'--calculate-cosine', 'False',
+						#'--checkpoint-step', '500',
+						'--eval-freq', '1']
+				print("Now running experiments on "+fault_name+" using "+model_name+" using command:")
+				print(' '.join(args))
+				results = subprocess.run(args, capture_output=True)
+				if results.returncode==0 and results.stdout != None:
+					with open('logs-paper2-cifar-' + batch_size + '-' + fault_name + '-' + model_name + '-40-' + str(i) + '-acc-' + acc_alpha,'w') as f:
+						f.write(results.stdout.decode())
+					print("finished")
+					print("========================")
+				else:
+					print(results.stderr.decode())
+					print("failed")
+					print("========================")
 
 model_names = ['geomedian','medofmeans','cwtm','mkrum5','normfilter']
 models = ['geometric_median','median_of_means','coor_wise_trimmed_mean','multi_krum','grad_norm']
@@ -62,39 +61,38 @@ for batch_size in batch_sizes:
 	for i in nums_faults:
 		for fault_type, fault_name in zip(fault_types, fault_names):
 			for model_name, model in zip(model_names, models):
-				for acc_alpha in acc_alphas:
-					args = ['mpirun', '-n', '41',
-							'python', 'distributed_nn.py',
-							'--batch-size=' + batch_size,
-							'--max-steps', '500',
-							'--epochs', '100',
-							'--network', 'LeNet',
-							'--mode', model, '--multi-krum-m','5',
-							'--dataset', 'CIFAR10',
-							'--approach', 'baseline',
-							'--err-mode', fault_type,
-							'--lr', '0.01',
-							'--train-dir', 'output/models/paper2/CIFAR10-LeNet/' + batch_size + '/' + fault_name + '/' + model_name + '/40-' + str(i) + '/',
-							'--accumulative', 'False',
-							#'--accumulative-alpha', '0.'+acc_alpha,
-							'--worker-fail', str(i),
-							'--fault-thrshld', str(i),
-							'--data-distribution', 'same',
-							'--calculate-cosine', 'False',
-							#'--checkpoint-step', '500',
-							'--eval-freq', '1']
-					print("Now running experiments on "+fault_name+" using "+model_name+" using command:")
-					print(' '.join(args))
-					results = subprocess.run(args, capture_output=True)
-					if results.returncode==0 and results.stdout != None:
-						with open('logs-paper2-cifar-' + batch_size + '-' + fault_name + '-' + model_name + '-40-' + str(i) + '-acc-' + acc_alpha,'w') as f:
-							f.write(results.stdout.decode())
-						print("finished")
-						print("========================")
-					else:
-						print(results.stderr.decode())
-						print("failed")
-						print("========================")
+				args = ['mpirun', '-n', '41',
+						'python', 'distributed_nn.py',
+						'--batch-size=' + batch_size,
+						'--max-steps', '500',
+						'--epochs', '100',
+						'--network', 'LeNet',
+						'--mode', model, '--multi-krum-m','5',
+						'--dataset', 'CIFAR10',
+						'--approach', 'baseline',
+						'--err-mode', fault_type,
+						'--lr', '0.01',
+						'--train-dir', 'output/models/paper2/CIFAR10-LeNet/' + batch_size + '/' + fault_name + '/' + model_name + '/40-' + str(i) + '/',
+						'--accumulative', 'False',
+						#'--accumulative-alpha', '0.'+acc_alpha,
+						'--worker-fail', str(i),
+						'--fault-thrshld', str(i),
+						'--data-distribution', 'same',
+						'--calculate-cosine', 'False',
+						#'--checkpoint-step', '500',
+						'--eval-freq', '1']
+				print("Now running experiments on "+fault_name+" using "+model_name+" using command:")
+				print(' '.join(args))
+				results = subprocess.run(args, capture_output=True)
+				if results.returncode==0 and results.stdout != None:
+					with open('logs-paper2-cifar-' + batch_size + '-' + fault_name + '-' + model_name + '-40-' + str(i) + '-acc-' + acc_alpha,'w') as f:
+						f.write(results.stdout.decode())
+					print("finished")
+					print("========================")
+				else:
+					print(results.stderr.decode())
+					print("failed")
+					print("========================")
 
 print()
 model_names = ['geomedian','medofmeans','cwtm','mkrum5']
@@ -107,12 +105,11 @@ for batch_size in batch_sizes:
 	for i in nums_faults:
 		for fault_type, fault_name in zip(fault_types, fault_names):
 			for model_name, model in zip(model_names, models):
-				for acc_alpha in acc_alphas:
-					args = 'python distributed_eval.py --model-dir output/models/paper2/MNIST-LeNet/'+batch_size+'/'+fault_name+'/'+model_name \
-						+'/40-'+str(i)+'/ --dataset CIFAR10 --network LeNet --eval-freq 1 > results-paper2-mnist-'+batch_size+'-'+fault_name+'-'+model_name+'-40-'+str(i)+' 2>&1 &'
-					print("Now evaluating "+fault_name+" using "+model_name+" using command:")
-					print(args)
-					results = subprocess.run(args, shell=True)
+				args = 'python distributed_eval.py --model-dir output/models/paper2/MNIST-LeNet/'+batch_size+'/'+fault_name+'/'+model_name \
+					+'/40-'+str(i)+'/ --dataset CIFAR10 --network LeNet --eval-freq 1 > results-paper2-cifar-'+batch_size+'-'+fault_name+'-'+model_name+'-40-'+str(i)+' 2>&1 &'
+				print("Now evaluating "+fault_name+" using "+model_name+" using command:")
+				print(args)
+				results = subprocess.run(args, shell=True)
 
 model_names = ['geomedian','medofmeans','cwtm','mkrum5','normfilter']
 models = ['geometric_median','median_of_means','coor_wise_trimmed_mean','multi_krum','grad_norm']
@@ -123,9 +120,8 @@ for batch_size in batch_sizes:
 	for i in nums_faults:
 		for fault_type, fault_name in zip(fault_types, fault_names):
 			for model_name, model in zip(model_names, models):
-				for acc_alpha in acc_alphas:
-					args = 'python distributed_eval.py --model-dir output/models/paper2/MNIST-LeNet/'+batch_size+'/'+fault_name+'/'+model_name \
-						+'/40-'+str(i)+'/ --dataset CIFAR10 --network LeNet --eval-freq 1 > results-paper2-mnist-'+batch_size+'-'+fault_name+'-'+model_name+'-40-'+str(i)+' 2>&1 &'
-					print("Now evaluating "+fault_name+" using "+model_name+" using command:")
-					print(args)
-					results = subprocess.run(args, shell=True)
+				args = 'python distributed_eval.py --model-dir output/models/paper2/MNIST-LeNet/'+batch_size+'/'+fault_name+'/'+model_name \
+					+'/40-'+str(i)+'/ --dataset CIFAR10 --network LeNet --eval-freq 1 > results-paper2-cifar-'+batch_size+'-'+fault_name+'-'+model_name+'-40-'+str(i)+' 2>&1 &'
+				print("Now evaluating "+fault_name+" using "+model_name+" using command:")
+				print(args)
+				results = subprocess.run(args, shell=True)
