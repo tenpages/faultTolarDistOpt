fb7845af9b995dafb0afd5cfc037bff079617dda
tenpages <tenpages66@gmail.com>
2021-01-02 21:05:21 -0500

    approximation dataset generator 1

--- distributed_eval.py
+++ distributed_eval.py
@@ -153,7 +153,10 @@ if __name__ == "__main__":
         testing_set=torch.load("linRegDataset5")
         test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
         data_shape = testing_set[0][0].size()[0]
-
+    elif args.dataset == "ApproxReg":
+        testing_set=torch.load("approximationDataset1")
+        test_loader = torch.utils.data.DataLoader(testing_set, batch_size=args.eval_batch_size, shuffle=True)
+        data_shape = testing_set[0][0].size()[0]
     print("testing set loaded.")
 
     kwargs_evaluator = {'model_dir': args.model_dir, 'eval_freq': args.eval_freq,
