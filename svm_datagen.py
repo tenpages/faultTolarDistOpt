a4a566a13886c2fc2774f24b1ac2c389f2c6debe
tenpages <tenpages66@gmail.com>
2021-01-23 23:07:12 -0500

    svm dataset

--- svm_datagen.py
+++ svm_datagen.py
@@ -5,10 +5,10 @@ import numpy as np
 X, Y = make_blobs(n_samples=1200, centers=2, cluster_std=1.2, random_state=1)
 X = (X - X.mean()) / X.std()
 Y[np.where(Y == 0)] = -1
-X, Y = torch.FloatTensor(X[:1000]), torch.FloatTensor(Y[:1000])
+X_train, Y_train = torch.FloatTensor(X[:1000]), torch.FloatTensor(Y[:1000])
 X_eval, Y_eval = torch.FloatTensor(X[1000:]), torch.FloatTensor(Y[1000:])
 
-dataset = torch.utils.data.TensorDataset(torch.tensor(X).float(), torch.tensor(Y).float().reshape(-1,1))
+dataset = torch.utils.data.TensorDataset(torch.tensor(X_train).float(), torch.tensor(Y_train).float().reshape(-1,1))
 with open("svmDataset","wb") as f:
     torch.save(dataset, f)
 dataset = torch.utils.data.TensorDataset(torch.tensor(X_eval).float(), torch.tensor(Y_eval).float().reshape(-1,1))
