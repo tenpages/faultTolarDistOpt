346fca3d618e07368c82f36e2f0f37c661e00939
tenpages <tenpages66@gmail.com>
2021-01-24 18:11:55 -0500

    set inital to 0 vector, save initial model

--- distributed_eval.py
+++ distributed_eval.py
@@ -77,8 +77,8 @@ class DistributedEvaluator(object):
             #self.results = np.array([[0.],[1.]], dtype = np.float64)
         else:
             self.true_minimum = kwargs['true_minimum']
-            self.results = np.array([[0.],[1.59999975],[1.89026521]], dtype = np.float64)
-            #self.results = np.insert([0.],[])
+            #self.results = np.array([[0.],[1.59999975],[1.89026521]], dtype = np.float64)
+            self.results = np.insert([0.],[1.],[2.], dtype = np.float64)
             self.out_of_w_list = []
             self.out_of_w_value = []
         if self.network_config == "FC":
