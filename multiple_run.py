eef91e610afab2e439ea879fcf972555cf87e61d
tenpages <tenpages66@gmail.com>
2021-04-14 15:03:50 -0400

    df new multirun

--- multiple_run.py
+++ multiple_run.py
@@ -17,45 +17,45 @@ df_betas = ['1', '2']
 df_sigma = 0.2
 #acc_alphas = ['20','40','60']
 
-for df_beta in df_betas:
-	for batch_size in batch_sizes:
-		for i in nums_faults:
-			for fault_type, fault_name in zip(fault_types, fault_names):
-				for model_name, model in zip(model_names, models):
-					args = ['mpirun', '-n', '10',
-							'python', 'distributed_nn.py',
-							'--batch-size=' + batch_size,
-							'--max-steps', '1000',
-							'--epochs', '100',
-							'--network', 'LeNet',
-							'--mode', model, '--multi-krum-m','2',
-							'--dataset', 'MNIST',
-							'--approach', 'baseline',
-							'--err-mode', fault_type,
-							'--lr', '0.01',
-							'--train-dir', 'output/models/df/df' + df_beta + '/' + fault_name + '/' + model_name + '/10-' + str(i) + '/',
-							'--accumulative', 'False',
-							#'--accumulative-alpha', '0.'+acc_alpha,
-							'--worker-fail', str(i),
-							'--fault-thrshld', str(i),
-							'--data-distribution', 'same',
-							'--calculate-cosine', 'False',
-							'--checkpoint-step', '0',
-							'--eval-freq', '1',
-							'--diff-privacy-param', df_beta,
-							'--diff-privacy-sigma', str(df_sigma)]
-					print("Now running experiments mpiron "+fault_name+" using "+model_name+" using command:")
-					print(' '.join(args))
-					results = subprocess.run(args, capture_output=True)
-					if results.returncode==0 and results.stdout != None:
-						with open('logs-df-MNIST-LeNet-df(' + df_beta + ',' + str(df_sigma) + ')' + batch_size + '-' + fault_name + '-' + model_name + '-10-' + str(i),'w') as f:
-							f.write(results.stdout.decode())
-						print("finished")
-						print("========================")
-					else:
-						print(results.stderr.decode())
-						print("failed")
-						print("========================")
+# for df_beta in df_betas:
+# 	for batch_size in batch_sizes:
+# 		for i in nums_faults:
+# 			for fault_type, fault_name in zip(fault_types, fault_names):
+# 				for model_name, model in zip(model_names, models):
+# 					args = ['mpirun', '-n', '10',
+# 							'python', 'distributed_nn.py',
+# 							'--batch-size=' + batch_size,
+# 							'--max-steps', '1000',
+# 							'--epochs', '100',
+# 							'--network', 'LeNet',
+# 							'--mode', model, '--multi-krum-m','2',
+# 							'--dataset', 'MNIST',
+# 							'--approach', 'baseline',
+# 							'--err-mode', fault_type,
+# 							'--lr', '0.01',
+# 							'--train-dir', 'output/models/df/df' + df_beta + '/' + fault_name + '/' + model_name + '/10-' + str(i) + '/',
+# 							'--accumulative', 'False',
+# 							#'--accumulative-alpha', '0.'+acc_alpha,
+# 							'--worker-fail', str(i),
+# 							'--fault-thrshld', str(i),
+# 							'--data-distribution', 'same',
+# 							'--calculate-cosine', 'False',
+# 							'--checkpoint-step', '0',
+# 							'--eval-freq', '1',
+# 							'--diff-privacy-param', df_beta,
+# 							'--diff-privacy-sigma', str(df_sigma)]
+# 					print("Now running experiments mpiron "+fault_name+" using "+model_name+" using command:")
+# 					print(' '.join(args))
+# 					results = subprocess.run(args, capture_output=True)
+# 					if results.returncode==0 and results.stdout != None:
+# 						with open('logs-df-MNIST-LeNet-df(' + df_beta + ',' + str(df_sigma) + ')' + batch_size + '-' + fault_name + '-' + model_name + '-10-' + str(i),'w') as f:
+# 							f.write(results.stdout.decode())
+# 						print("finished")
+# 						print("========================")
+# 					else:
+# 						print(results.stderr.decode())
+# 						print("failed")
+# 						print("========================")
 
 print()
 for df_beta in df_betas:
