c41ac415cc43245c2dc37d75585eebaea54be3ea
tenpages <tenpages66@gmail.com>
2021-05-02 23:45:04 -0400

    update multirun for df

--- distributed_nn.py
+++ distributed_nn.py
@@ -218,6 +218,22 @@ def load_data(dataset, seed, args, rank, world_size, adversaries):
         test_loader = None
         return train_loader, training_set, test_loader
 
+    elif dataset == "WDBC":
+        if rank==0:
+            training_set = torch.load("wdbcDataset")
+            train_loader = torch.utils.data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+        else:
+            if args.data_distribution == 'distributed':
+                group_size = int(400 / (world_size - 1))
+                tmp_set = torch.load("wdbcDataset")[group_size*(rank-1):group_size*rank]
+                training_set = torch.utils.data.TensorDataset(tmp_set[0], tmp_set[1])
+                train_loader = torch.utils.data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+            elif args.data_distribution == 'same':
+                torch.manual_seed(TORCH_SEED_+rank)
+                training_set = torch.load("wdbcDataset")
+                train_loader = torch.utils.data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+        test_loader = None
+        return train_loader, training_set, test_loader
     #print("here2")
     return None, None, None
 
