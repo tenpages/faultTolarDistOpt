f76c0a25b70d497f9d0e13190773af83e043c2b8
tenpages <tenpages66@gmail.com>
2021-05-17 01:24:41 -0400

    update async comm time check

--- multiple_run.py
+++ multiple_run.py
@@ -16,79 +16,78 @@ batch_sizes = ['128']
 #acc_alpha = '60'
 #acc_alphas = ['20','40','60']
 
-for batch_size in batch_sizes:
-	for i in nums_faults:
-		for fault_type, fault_name in zip(fault_types, fault_names):
-			for model_name, model in zip(model_names, models):
-				args = ['mpirun', '-n', '21',
-						'python', 'distributed_nn.py',
-						'--batch-size=' + batch_size,
-						'--max-steps', '1000',
-						'--epochs', '100',
-						'--network', 'LeNet',
-						'--mode', model,
-						'--dataset', 'MNIST',
-						'--approach', 'baseline',
-						'--err-mode', fault_type,
-						'--lr', '0.01',
-						'--train-dir', 'output/models/async/' + fault_name + '-' + str(i) + '/',
-						'--accumulative', 'False',
-						'--worker-fail', str(i),
-						'--fault-thrshld', str(i),
-						'--data-distribution', 'same',
-						'--calculate-cosine', 'False',
-						'--checkpoint-step', '0',
-						'--eval-freq', '1',
-						'--diff-privacy-param', '0']
-				print("Now running experiments mpiron "+fault_name+" using "+model_name+" using command:")
-				print(' '.join(args))
-				results = subprocess.run(args, capture_output=True)
-				if results.returncode==0 and results.stdout != None:
-					with open('logs-async-MNIST-LeNet-' + batch_size + '-' + model_name + '-20-' + str(i),'w') as f:
-						f.write(results.stdout.decode())
-					print("finished")
-					print("========================")
-				else:
-					print(results.stderr.decode())
-					print("failed")
-					print("========================")
+# for batch_size in batch_sizes:
+# 	for i in nums_faults:
+# 		for fault_type, fault_name in zip(fault_types, fault_names):
+# 			for model_name, model in zip(model_names, models):
+# 				args = ['mpirun', '-n', '21',
+# 						'python', 'distributed_nn.py',
+# 						'--batch-size=' + batch_size,
+# 						'--max-steps', '1000',
+# 						'--epochs', '100',
+# 						'--network', 'LeNet',
+# 						'--mode', model,
+# 						'--dataset', 'MNIST',
+# 						'--approach', 'baseline',
+# 						'--err-mode', fault_type,
+# 						'--lr', '0.01',
+# 						'--train-dir', 'output/models/async/' + fault_name + '-' + str(i) + '/',
+# 						'--accumulative', 'False',
+# 						'--worker-fail', str(i),
+# 						'--fault-thrshld', str(i),
+# 						'--data-distribution', 'same',
+# 						'--calculate-cosine', 'False',
+# 						'--checkpoint-step', '0',
+# 						'--eval-freq', '1',
+# 						'--diff-privacy-param', '0']
+# 				print("Now running experiments mpiron "+fault_name+" using "+model_name+" using command:")
+# 				print(' '.join(args))
+# 				results = subprocess.run(args, capture_output=True)
+# 				if results.returncode==0 and results.stdout != None:
+# 					with open('logs-async-MNIST-LeNet-' + batch_size + '-' + model_name + '-20-' + str(i),'w') as f:
+# 						f.write(results.stdout.decode())
+# 					print("finished")
+# 					print("========================")
+# 				else:
+# 					print(results.stderr.decode())
+# 					print("failed")
+# 					print("========================")
 
 for batch_size in batch_sizes:
-	for i in nums_faults:
-		for fault_type, fault_name in zip(fault_types, fault_names):
-			for model_name, model in zip(model_names, models):
-				args = ['mpirun', '-n', '21',
-						'python', 'distributed_nn.py',
-						'--batch-size=' + batch_size,
-						'--max-steps', '1000',
-						'--epochs', '100',
-						'--network', 'LeNet',
-						'--mode', model,
-						'--dataset', 'MNIST',
-						'--approach', 'baseline',
-						'--err-mode', fault_type,
-						'--lr', '0.01',
-						'--train-dir', 'output/models/async/sync/',
-						'--accumulative', 'False',
-						'--worker-fail', '0',
-						'--fault-thrshld', '0',
-						'--data-distribution', 'same',
-						'--calculate-cosine', 'False',
-						'--checkpoint-step', '0',
-						'--eval-freq', '1',
-						'--diff-privacy-param', '0']
-				print("Now running experiments mpiron "+fault_name+" using "+model_name+" using command:")
-				print(' '.join(args))
-				results = subprocess.run(args, capture_output=True)
-				if results.returncode==0 and results.stdout != None:
-					with open('logs-async-MNIST-LeNet-' + batch_size + '-sync-20-' + str(i),'w') as f:
-						f.write(results.stdout.decode())
-					print("finished")
-					print("========================")
-				else:
-					print(results.stderr.decode())
-					print("failed")
-					print("========================")
+	for fault_type, fault_name in zip(fault_types, fault_names):
+		for model_name, model in zip(model_names, models):
+			args = ['mpirun', '-n', '21',
+					'python', 'distributed_nn.py',
+					'--batch-size=' + batch_size,
+					'--max-steps', '1000',
+					'--epochs', '100',
+					'--network', 'LeNet',
+					'--mode', model,
+					'--dataset', 'MNIST',
+					'--approach', 'baseline',
+					'--err-mode', fault_type,
+					'--lr', '0.01',
+					'--train-dir', 'output/models/async/sync/',
+					'--accumulative', 'False',
+					'--worker-fail', '0',
+					'--fault-thrshld', '0',
+					'--data-distribution', 'same',
+					'--calculate-cosine', 'False',
+					'--checkpoint-step', '0',
+					'--eval-freq', '1',
+					'--diff-privacy-param', '0']
+			print("Now running experiments mpiron "+fault_name+" using "+model_name+" using command:")
+			print(' '.join(args))
+			results = subprocess.run(args, capture_output=True)
+			if results.returncode==0 and results.stdout != None:
+				with open('logs-async-MNIST-LeNet-' + batch_size + '-sync-20-0','w') as f:
+					f.write(results.stdout.decode())
+				print("finished")
+				print("========================")
+			else:
+				print(results.stderr.decode())
+				print("failed")
+				print("========================")
 
 print()
 model_names = ['async']
@@ -109,11 +108,10 @@ for batch_size in batch_sizes:
 				results = subprocess.run(args, shell=True)
 
 for batch_size in batch_sizes:
-	for i in nums_faults:
-		for fault_type, fault_name in zip(fault_types, fault_names):
-			for model_name, model in zip(model_names, models):
-				args = 'python distributed_eval.py --model-dir output/models/async/sync'\
-						+'/ --dataset MNIST --network LeNet --eval-freq 1 > "results-async-MNIST-LeNet-'+batch_size+'-sync-20-'+str(i)+'" 2>&1 &'
-				print("Now evaluating "+fault_name+" using "+model_name+" using command:")
-				print(args)
-				results = subprocess.run(args, shell=True)
+	for fault_type, fault_name in zip(fault_types, fault_names):
+		for model_name, model in zip(model_names, models):
+			args = 'python distributed_eval.py --model-dir output/models/async/sync'\
+					+'/ --dataset MNIST --network LeNet --eval-freq 1 > "results-async-MNIST-LeNet-'+batch_size+'-sync-20-0" 2>&1 &'
+			print("Now evaluating "+fault_name+" using "+model_name+" using command:")
+			print(args)
+			results = subprocess.run(args, shell=True)
